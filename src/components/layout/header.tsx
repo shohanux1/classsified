ce2572f01e3dba8380db6bce6d26a215b12a0955
Md Abdullah Al Noman <56474819+devnoman0@users.noreply.github.com>
2024-02-24 02:17:10 +0600

    Fixed header border for mobile view

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,20 +4,22 @@ import { Button } from "@/components/ui/button";
 
 const Header = () => {
   return (
-    <div className="flex flex-col md:flex-row items-start gap-4 md:items-center justify-between max-w-5xl py-4 mx-auto px-4 w-full">
-      <div className="space-y-0.5">
-        <Link className="text-3xl font-semibold text-primary" href={"/"}>
-          Logo
-        </Link>
-        <h1 className="text-xs font-light text-zinc-700">
-          Skip the games. Get satisfaction.
-        </h1>
-      </div>
+    <div className="border-b">
+      <div className="flex flex-col md:flex-row items-start gap-4 md:items-center justify-between max-w-5xl py-4 mx-auto px-4 w-full">
+        <div className="space-y-0.5">
+          <Link className="text-3xl font-semibold text-primary" href={"/"}>
+            Logo
+          </Link>
+          <h1 className="text-xs font-light text-zinc-700">
+            Skip the games. Get satisfaction.
+          </h1>
+        </div>
 
-      <div className="w-full md:max-w-sm">
-        <Button className="w-full" size={"lg"}>
-          Login / Signup
-        </Button>
+        <div className="w-full md:max-w-sm">
+          <Button className="w-full" size={"lg"}>
+            Login / Signup
+          </Button>
+        </div>
       </div>
     </div>
   );
