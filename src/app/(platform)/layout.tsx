02de89748f659241dd5ceee2e4f63232690507e6
Md Abdullah Al Noman <56474819+devnoman0@users.noreply.github.com>
2024-03-08 03:05:55 +0600

    :(

--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -1,7 +1,7 @@
+import Filter from "@/components/layout/Filter";
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
 import React from "react";
-import Header from "../../components/layout/Header";
-import Footer from "../../components/layout/Footer";
-import Filter from "../../components/layout/Filter";
 
 //custom component
 
