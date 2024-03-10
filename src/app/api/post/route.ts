369e7c2deb8abddbdda2f6d0b2b70b4728a241d3
Md Abdullah Al Noman <56474819+devnoman0@users.noreply.github.com>
2024-03-11 01:39:10 +0600

    Added scripts on package

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -3,80 +3,12 @@ import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
-// export async function POST(req: any) {
-//   const session = await getServerSession(authOptions);
-
-//   if (!session) {
-//     return null;
-//   }
-
-//   try {
-//     const body = await req.json();
-//     const {
-//       countryId,
-//       stateId,
-//       cityId,
-//       title,
-//       age,
-//       location,
-//       categoryId,
-//       serviceGender,
-//       postBody,
-//       contact,
-//       receiveCall,
-//       pictures,
-//       published,
-//       ethnicity,
-//       hairColor,
-//       tatoo,
-//       seeClient,
-//       workingHours,
-//       paymentMethods,
-//       bdsmActivity,
-//       visibleToGoogle,
-//       disclaimer,
-//     } = await body;
-
-//     const post = await db.post.create({
-//       data: {
-//         countryId: 1,
-//         stateId,
-//         cityId,
-//         title,
-//         age,
-//         location,
-//         categoryId,
-//         serviceGender,
-//         postBody,
-//         contact,
-//         receiveCall,
-//         pictures: [""],
-//         published,
-//         authorId: Number(session?.user.id),
-//         ethnicity,
-//         hairColor,
-//         tatoo,
-//         seeClient,
-//         workingHours,
-//         paymentMethods,
-//         bdsmActivity,
-//         visibleToGoogle,
-//         disclaimer,
-//       },
-//     });
-
-//     return NextResponse.json({ status: 201 });
-//   } catch (error) {
-//     return NextResponse.json(error, { status: 500 });
-//   }
-// }
-
 export async function POST(request: Request) {
-  // const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-  // if (!session) {
-  //   return null;
-  // }
+  if (!session) {
+    return null;
+  }
 
   try {
     const body = await request.json();
@@ -120,8 +52,8 @@ export async function POST(request: Request) {
         receiveCall,
         pictures: [""],
         published,
-        // authorId: Number(session?.user.id),
-        authorId: 1,
+        authorId: Number(session?.user.id),
+        // authorId: 1,
         ethnicity,
         hairColor,
         tatoo,
