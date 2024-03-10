b890253755a272c252f327c1e86d98684f0b4518
Md Abdullah Al Noman <56474819+devnoman0@users.noreply.github.com>
2024-03-11 01:15:54 +0600

    Change post page

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -3,15 +3,83 @@ import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
-export async function POST(req: any) {
-  const session = await getServerSession(authOptions);
+// export async function POST(req: any) {
+//   const session = await getServerSession(authOptions);
 
-  if (!session) {
-    return null;
-  }
+//   if (!session) {
+//     return null;
+//   }
+
+//   try {
+//     const body = await req.json();
+//     const {
+//       countryId,
+//       stateId,
+//       cityId,
+//       title,
+//       age,
+//       location,
+//       categoryId,
+//       serviceGender,
+//       postBody,
+//       contact,
+//       receiveCall,
+//       pictures,
+//       published,
+//       ethnicity,
+//       hairColor,
+//       tatoo,
+//       seeClient,
+//       workingHours,
+//       paymentMethods,
+//       bdsmActivity,
+//       visibleToGoogle,
+//       disclaimer,
+//     } = await body;
+
+//     const post = await db.post.create({
+//       data: {
+//         countryId: 1,
+//         stateId,
+//         cityId,
+//         title,
+//         age,
+//         location,
+//         categoryId,
+//         serviceGender,
+//         postBody,
+//         contact,
+//         receiveCall,
+//         pictures: [""],
+//         published,
+//         authorId: Number(session?.user.id),
+//         ethnicity,
+//         hairColor,
+//         tatoo,
+//         seeClient,
+//         workingHours,
+//         paymentMethods,
+//         bdsmActivity,
+//         visibleToGoogle,
+//         disclaimer,
+//       },
+//     });
+
+//     return NextResponse.json({ status: 201 });
+//   } catch (error) {
+//     return NextResponse.json(error, { status: 500 });
+//   }
+// }
+
+export async function POST(request: Request) {
+  // const session = await getServerSession(authOptions);
+
+  // if (!session) {
+  //   return null;
+  // }
 
   try {
-    const body = await req.json();
+    const body = await request.json();
     const {
       countryId,
       stateId,
@@ -52,7 +120,8 @@ export async function POST(req: any) {
         receiveCall,
         pictures: [""],
         published,
-        authorId: Number(session?.user.id),
+        // authorId: Number(session?.user.id),
+        authorId: 1,
         ethnicity,
         hairColor,
         tatoo,
@@ -65,8 +134,12 @@ export async function POST(req: any) {
       },
     });
 
-    return NextResponse.json({ status: 201 });
-  } catch (error) {
-    return NextResponse.json(error, { status: 500 });
+    return new Response("Your post has been successfully created.", {
+      status: 201,
+    });
+  } catch (error: any) {
+    return new Response(`Error: ${error.message}`, {
+      status: 400,
+    });
   }
 }
